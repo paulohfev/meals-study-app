afb2052602d0731f7a27d01813c9a3c46f2e4c59
Paulo Henrique Evangelista <phfernandesev@gmail.com>
2023-02-21 23:32:13 -0300

    Register categories screen as home

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -6,20 +6,23 @@ class CategoriesScreen extends StatelessWidget {
   @override
   // "slivers" are scrollable screens in flutter
   Widget build(BuildContext context) {
-    return GridView(
-      children: DUMMY_CATEGORIES
-          .map(
-            (categoryData) => CategoryItem(
-              categoryData.title,
-              categoryData.color,
-            ),
-          )
-          .toList(),
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
+    return Scaffold(
+      appBar: AppBar(title: const Text('DeliMeal')),
+      body: GridView(
+        children: DUMMY_CATEGORIES
+            .map(
+              (categoryData) => CategoryItem(
+                categoryData.title,
+                categoryData.color,
+              ),
+            )
+            .toList(),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
       ),
     );
   }
